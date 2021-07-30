9e40d926e3a6588e59bd1dd8ba5f2e255467c514
tomafoks <tomafoks@mail.ru>
2021-07-30 17:10:42 +0500

    fix login

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -1,28 +1,31 @@
 <?php
 
-class UserController extends Controller{
-    function render(){
+class UserController extends Controller
+{
+    function render()
+    {
 
-        $template=new Template;
+        $template = new Template;
         echo $template->render('login.htm');
     }
 
-    function beforeroute(){
+    function beforeroute()
+    {
     }
 
-    function authenticate() {
+    function authenticate()
+    {
 
         $username = $this->f3->get('POST.username');
         $password = $this->f3->get('POST.password');
 
         $user = new User($this->db);
         $user->getByName($username);
-
-        if($user->dry()) {
+        
+        if ($user->dry()) {
             $this->f3->reroute('/login');
         }
-
-        if(password_verify($password, $user->password)) {
+        if (password_verify($password, $user->password)) {
             $this->f3->set('SESSION.user', $user->username);
             $this->f3->reroute('/');
         } else {
